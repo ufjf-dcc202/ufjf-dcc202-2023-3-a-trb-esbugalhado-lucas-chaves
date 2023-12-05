622ed14a80f3300e39e0546baa4f24c646550d6e
Lucas Chaves <lucasvchaves@gmail.com>
2023-12-05 17:14:23 -0300

    Adiciona pasta source com modulo que crias as tabelas

---
+++ src/table.js
@@ -0,0 +1,21 @@
+// funcao para criar um tabuleiro 3x3
+
+function createBoard(tableID) {
+    var table = document.getElementById(tableID);
+
+    for(var i = 0; i < 3; i++) {
+        var row = table.insertRow(i);
+        for(var j = 0; j < 3; j++) {
+            var cell = row.insertCell(j);
+            cell.addEvetListener("click", testCell);
+        }
+    }
+}
+
+// funcao placeholder enquanto nao tem a logica do jogo
+function testCell() {
+    this.textContent = this.textContent === "" ? "A" : "B";
+}
+
+createBoard("player-table");
+createBoard("cpu-table");
