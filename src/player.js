3ebae1898b48abc0e71271da1a3428933bfa2e72
Lucas Chaves <lucasvchaves@gmail.com>
2023-12-13 14:48:56 -0300

    Adiciona a propriedade pontos a classe player, e metodos para escolher uma coluna aleatoria e para atualizar a quantidade de pontos

--- src/player.js
+++ src/player.js
@@ -3,12 +3,22 @@ export default class Player {
         this.board = board;
         this.dice = dice;
         this.isPlayer = isPlayer;
+        this.points = 0;
     }
 
     placeDiceNum(col) {
         if(this.board.checkCol(col)){
             this.board.updateBoardData(this.dice.currNum);
         }
+    }
+
+    placeOnRandCol() {
+        let randCol = Math.floor(Math.random() * 3);
+        this.placeDiceNum(randCol);
+    }
+
+    updatePoints(points) {
+        this.points += points;
     }
 
 }
